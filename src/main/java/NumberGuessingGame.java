ba81e6f3d7a83d6f7f53ca9c5f9ca581d695227d
lior <liorshalmay1@gmail.com>
2024-10-21 07:18:57 +0300

    added help feature

--- src/main/java/NumberGuessingGame.java
+++ src/main/java/NumberGuessingGame.java
@@ -1,9 +1,11 @@
-
+import java.util.ArrayList;
 
 public class NumberGuessingGame implements Game {
     int number;
 
     boolean wasGuessed;
+    private int closest_guest_top;
+    private int closest_guest_bottom;
 
     /**
      * Initialize with a fixed number.
@@ -13,6 +15,8 @@ public class NumberGuessingGame implements Game {
     public NumberGuessingGame(int number) {
         this.number = number;
         wasGuessed = false;
+        this.closest_guest_bottom = 1;
+        this.closest_guest_top = 100;
     }
 
     /**
@@ -34,21 +38,26 @@ public class NumberGuessingGame implements Game {
     @Override
     public StatusUpdate playerMove(Player player, String word, long time) {
         StatusUpdate update = new StatusUpdate();
-        int guess;
-        try {
-            guess = Integer.parseInt(word);
-        } catch (Exception e){
-            update.addMessage("something went wrong....");
-            return update;
-        }
 
-        if (guess == number) {
-            update.addMessage("Yay! " + player.getName() + " has guessed the number!");
-            wasGuessed = true;
-        } else if (guess > number) {
-            update.addMessage(player, "Your guess is too high");
+        if (word.equals("?")) {
+            this.offer_help(player, update);
         } else {
-            update.addMessage(player, "Your guess is too low");
+            int player_guess = Integer.parseInt(word);
+
+            if (player_guess == this.number) {
+                update.addMessage("Yay! " + player.getName() + " has guessed the number!");
+                wasGuessed = true;
+            } else if (player_guess > this.number) {
+                update.addMessage(player, "Your guess is too high");
+                if (this.closest_guest_top > player_guess){
+                    this.closest_guest_top = player_guess;
+                }
+            } else {
+                update.addMessage(player, "Your guess is too low");
+                if (this.closest_guest_bottom < player_guess){
+                    this.closest_guest_bottom = player_guess;
+                }
+            }
         }
         return update;
     }
@@ -65,4 +74,8 @@ public class NumberGuessingGame implements Game {
         return wasGuessed;
     }
 
+    private void offer_help(Player player, StatusUpdate update){
+        update.addMessage(player, "From your guesses so far it can be concluded that:");
+        update.addMessage(player, String.format("The number is between %d and %d", this.closest_guest_bottom, this.closest_guest_top));
+    }
 }
